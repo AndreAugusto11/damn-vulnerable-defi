2688c289999b67fa7ee8f57c4e100c350461b31c
André Augusto <andre.augusto@tecnico.ulisboa.pt>
2023-01-13 22:58:19 +0000

    Solution #1 - Unstoppable
    Signed-off-by: André Augusto <andre.augusto@tecnico.ulisboa.pt>

--- hardhat.config.js
+++ hardhat.config.js
@@ -22,3 +22,11 @@ module.exports = {
       ],
     }
 }
+
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
